8bf18a77d55ce06212d6edff24b323577b678ee2
Ahmed <ahmedanari2001@gmail.com>
2024-10-13 23:44:40 +0500

    added new cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,13 +6,13 @@ import Categories from "@/_components/Categories";
 export default function Home() {
   return (
     <section className="bg-[#181313] h-[150vh] flex flex-col items-center px- py-6">
-      <div className="mb-10 bg-gradient-to-r w-[90%] from-[#7f7362] to-[#52422b] px-14 py-20 rounded-2xl">
-        <h1 className="text-4xl font-semibold text-white mb-10">
+      <div className="mb-10 bg-gradient-to-r w-[90%] from-[#7f7362] to-[#52422b] px-4 md:px-14 py-20 rounded-2xl">
+        <h1 className="text-2xl md:text-4xl font-semibold text-white mb-10">
           Book everything, now.
         </h1>
         <label className="relative">
           <input
-            className="w-1/3 py-2 placeholder:text-gray-700 rounded-full px-8"
+            className="w-[70%] lg:w-1/2 py-2 placeholder:text-gray-700 rounded-full px-8"
             placeholder="Search"
             id="search"
             name="search"
@@ -21,9 +21,9 @@ export default function Home() {
           ></input>
           <IoSearchOutline className="absolute text-xl top-0.5 left-2" />
         </label>
-        <div className="text-sm text-white mt-2">
-          <CiLocationOn className="inline mr-1 text-xl " />
-          <span>New York City, United States</span>
+        <div className="text-sm flex items-center gap-2 text-white mt-2">
+          <CiLocationOn className="text-xl " />
+          <span className=""> New York City, United States</span>
         </div>
       </div>
 
